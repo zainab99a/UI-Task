2ccb9586773c09a8f829eb9b4ff4697e744b0be7
zainab99a <amanhal729@gmail.com>
2024-09-22 17:12:29 +0300

    edit code and install zustand

--- src/sections/HeroPage.jsx
+++ src/sections/HeroPage.jsx
@@ -11,7 +11,7 @@ export default function HeroPage() {
                     your business
                 </h1>
             </div>
-            <button className='flex  items-center bg-yellow-400 rounded-3xl px-5 py-2 text-indigo-950 font-semibold mt-5' >CONTACT US  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#1F2937" class="w-6 h-6 ml-1 text-yellow-500 transition-all"><path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"></path></svg></button>
+            <a href='#contact' className='flex  items-center justify-center bg-yellow-400 rounded-2xl px-2 py-2 w-48  text-indigo-950 font-semibold mt-5' >CONTACT US  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#1F2937" class="w-6 h-6 ml-1 text-yellow-500 transition-all"><path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"></path></svg></a>
         </div>
     )
 }
