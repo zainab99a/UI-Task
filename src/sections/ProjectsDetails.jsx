31de1cdf0f602786d1689d6adbe2bfee7f04d036
zainab99a <amanhal729@gmail.com>
2024-09-23 09:42:40 +0300

    add detalis file

---
+++ src/sections/ProjectsDetails.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const ProjectsDetails = () => {
+  return (
+    <div className='h-[100vh]'>
+      <h1>Project Details</h1>
+     
+    </div>
+  );
+};
+
+export default ProjectsDetails; 
