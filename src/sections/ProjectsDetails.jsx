a0054ca0780a798fc5cd0b62a90eea52d9e72f81
zainab99a <amanhal729@gmail.com>
2024-09-23 16:24:13 +0300

    add detalis file

--- src/sections/ProjectsDetails.jsx
+++ src/sections/ProjectsDetails.jsx
@@ -1,13 +1,31 @@
-
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ProjectsDetails = () => {
+  const { data } = useQuery({
+    queryKey: ["projects"],
+    enabled: false,
+  });
+
   return (
     <div className='h-[100vh]'>
-      <h1>Project Details</h1>
-     
+      <div className="grid grid-rows-4 lg:grid-rows-none lg:grid-cols-4 gap-4 mt-8 mx-28">
+        {data && data.map((item) => (
+          <Link to={`/card/${item.id}`} state={{ data: item }} key={item.id}>
+            <div>
+              <img
+                className="w-full h-full rounded-lg"
+                src={item.Image.url}
+                alt=""
+              />
+              <h1 className='text-center mt-4 text-white text-2xl'>{item.title}</h1>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
 
-export default ProjectsDetails; 
+export default ProjectsDetails;
